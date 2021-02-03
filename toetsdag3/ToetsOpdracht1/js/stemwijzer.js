a5ab428d5b76ddd6c35cf6d43dbcb17f056d8206
Sven <99063753@mydavinci.nl>
2021-02-03 09:38:54 +0100

    0.0.1 \\ basis opgezet

---
+++ toetsdag3/ToetsOpdracht1/js/stemwijzer.js
@@ -0,0 +1,6 @@
+var eensButton = documet.getElementById('eensButton');
+var oneensButton = documet.getElementById('oneensButton');
+
+function test() {
+	if (oneensButton.checked) {alert("werkt")}
+}
